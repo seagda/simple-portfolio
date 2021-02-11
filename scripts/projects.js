b31da8cddf125c6d2b22257b5fc1962c1836cde7
Shea Mullaney <seagda@gmail.com>
2021-02-11 03:33:29 -0800

    portfolio images done

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,60 +6,60 @@ const projectList = [
 //     userStory: "",
 //     deplUrl: "",
 //     repoUrl: "",
-//     pic2: ""
+//     award: ""
 //   },
   {
     title: "Password Generator",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    desc: "Random password generator built in JavaScript utilizing browser prompts to set user parameters.",
+    media: "/assets/img/pic-pwd.png",
+    userStory: "AS AN employee with access to sensitive data, I WANT to randomly generate a password that meets certain criteria, SO THAT I can create a strong password that provides greater security.",
+    deplUrl: "https://seagda.github.io/password-gen/",
+    repoUrl: "https://github.com/seagda/password-gen",
+    award: ""
   },
   {
-    title: "",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    title: "The Full Tankard",
+    desc: "Streamlined, responsive web application that allows bar managers to find and save information about local beers and craft breweries.",
+    media: "/assets/img/pic-tankard.png",
+    userStory: "Bar managers have to make purchasing decisions, sometimes on the fly, sometimes mapped out weeks or months in advance. As such, they need access to information about the available offerings from local breweries.",
+    deplUrl: "https://seagda.github.io/the-full-tankard/",
+    repoUrl: "https://github.com/seagda/the-full-tankard",
+    award: "Instructor Award for Best UI/UX, UW Coding Bootcamp Fall 2020"
   },
   {
-    title: "",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    title: "Turn the Tables",
+    desc: "A full-stack app with Node, Express, JQuery, and Handlebars to allow restaurants to review their guests, inverting the traditional reviews site model.",
+    media: "/assets/img/pic-ttt.png",
+    userStory: "We allow businesses to create and review their customers. Businesses WANT TO create their profile, THEN log in to create customers, THEN create reviews for customers, THEN update and delete those reviews and customers. Customers WANT TO claim their profile THEN log in to edit their profile, THEN comment on reviews left about them, OR TO message the business, AND receive rewards from multiple businesses.",
+    deplUrl: "https://peaceful-gorge-36427.herokuapp.com/",
+    repoUrl: "https://github.com/tmkeller/eightysixed-app",
+    award: "Instructor Award for Best Presentation, UW Coding Bootcamp Fall 2020"
   },
   {
-    title: "",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    title: "Are You a Replicant?",
+    desc: "Timed quiz built with classic JavaScript, CSS, and HTML. Designed as a variation of the ever popular quirky personality quiz a la BuzzFeed, this quiz pays homage to the Voight-Kampff test from the Blade Runner franchise.",
+    media: "/assets/img/pic-replicant.png",
+    userStory: "AS a user, I WANT to take a timed personality quiz that entertains me until the quiz is over. WHEN the quiz is over, I can save my initials and scores, THEN view it within a list of previous scores.",
+    deplUrl: "https://seagda.github.io/code-quiz/",
+    repoUrl: "https://github.com/seagda/code-quiz",
+    award: ""
   },
   {
-    title: "",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    title: "OpenWeather API Display",
+    desc: "Weather dashboard built in Bootstrap 4, accessing the OpenWeather API to allow users to search for a city, see current weather conditions and forecast data, and store previous searches in localStorage.",
+    media: "/assets/img/pic-weather.png",
+    userStory: "AS A traveler I WANT to find out current weather conditions in various cities around the globe, and I WANT to have a five day forecast SO THAT I can plan my travel activities.",
+    deplUrl: "https://seagda.github.io/weather-display/",
+    repoUrl: "https://github.com/seagda/weather-display",
+    award: ""
   },
   {
-    title: "",
-    desc: "",
-    media: "",
-    userStory: "",
-    deplUrl: "",
-    repoUrl: "",
-    pic2: ""
+    title: "Employee Tracker CLI",
+    desc: "Command-line Interface application is built using Node.js and Inquirer NPM to manipulate employee data within a MySQL database that's pre-populated with tables and seed data.",
+    media: "/assets/img/pic-employee.png",
+    userStory: "AS A traveler I WANT to find out current weather conditions in various cities around the globe, and I WANT to have a five day forecast SO THAT I can plan my travel activities.",
+    deplUrl: "https://www.youtube.com/watch?v=JX8vAIFfYdk&feature=youtu.be",
+    repoUrl: "https://github.com/seagda/employee-tracker",
+    award: ""
   }
 ]
