13cb10fd0c1ab27524cb106645f45875af36d484
Shea Mullaney <seagda@gmail.com>
2021-03-04 08:58:08 -0800

    portfolio and resume completed

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -3,28 +3,32 @@
 $(document).ready(init);
 
 function init(){
-// Once loaded, iterates over the array and creates a card for each project object 
+// Once loaded, iterates over the array and creates a card for each project  
+// TODO: include data-index attribute for index in the projects array for the corresponding project
     projectList.forEach((project, index) =>{
-        
+
     let projectCard = $(`
-    
     <div class="card myProject">
-      <img src="${project.media}" class="card-img-top" alt="${project.title}">
+        <img src="${project.media}" class="card-img-top" alt="${project.title}">
         <div class="card-body">
           <h5 class="card-title">${project.title}</h5>
           <p class="card-text">${project.desc}<br />
-          <a href="#" data-index="${index}" class="btn btn-primary" data-toggle="modal" data-target="#projectDisp">Read More</a>
+          <a href="#" data-id="${index}" class="btn btn-primary click-modal" data-toggle="modal" data-target="#projectDisp">Read More</a>
         </div>
     </div>
     `);
     $("#cards-row").append(projectCard);
   })
+
+// pass corresponding data into that modal based on the index position
+$(".click-modal").click(event => {
+    let projIdx = $(event.target).data("id");
+    $("#disp-title").text(projectList[projIdx].title);
+    $("#disp-desc").text(projectList[projIdx].desc);
+    $("#depl-link").text(projectList[projIdx].deplUrl);
+    $("#repo-link").text(projectList[projIdx].repoUrl);
+    $("#disp-story").text(projectList[projIdx].userStory);
+    $("#disp-award").text(projectList[projIdx].award)
+  });
 };
-
-
-
-
-
-// TODO: include data-index attribute for index in the projects array of the corresponding object for that peroject
-// TODO: pass corresponding data into that modal based on the index position
 
